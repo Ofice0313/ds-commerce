6b6fb2d09368e0227a9f31ea85c296f717558e04
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-03-04 09:11:03 +0200

    Components of details page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 
-import computerImg from './assets/images/computer.svg'
+
 import './App.css'
 import HeaderClient from './components/HeaderClient'
+import ButtonPrimary from './components/ButtonPrimary'
+import ButtonInverse from './components/ButtonInverse'
+import ProductDetailsCard from './components/ProductDetailsCard'
 
 function App() {
 
@@ -9,39 +12,11 @@ function App() {
     <>
       <HeaderClient />
       <main>
-
         <section id="product-details-section" className="dsc-container">
-          <div className="dsc-card dsc-mb20">
-            <div className="dsc-product-details-top dsc-line-bottom">
-              <img src={computerImg} alt="Computer" />
-            </div>
-            <div className="dsc-product-details-bottom">
-              <h3>R$ 5000,00</h3>
-              <h4>Computer Gamer XT</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tenetur explicabo perferendis autem nam culpa harum iusto obcaecati eveniet
-                voluptas fugit aspernatur corporis dicta delectus voluptatum
-                soluta, dolores eaque neque? Qui!
-              </p>
-              <div className="dsc-category-container">
-                <div className="dsc-category">
-                  <span>Electrónicos</span>
-                </div>
-                <div className="dsc-category">
-                  <span>Computadores</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
+          <ProductDetailsCard />
           <div className="dsc-btn-page-container">
-            <div className="dsc-btn dsc-btn-blue">
-              Comprar
-            </div>
-            <div className="dsc-btn dsc-btn-white">
-              Início
-            </div>
+            <ButtonPrimary />
+            <ButtonInverse />
           </div>
         </section>
       </main>
---
+++ frontend/src/components/ProductDetailsCard/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css'
+import computerImg from '../../assets/images/computer.svg'
+import ProductCategory from '../ProductCategory'
+
+export default function ProductDetailsCard () {
+    return (
+        <div className="dsc-card dsc-mb20">
+            <div className="dsc-product-details-top dsc-line-bottom">
+              <img src={computerImg} alt="Computer" />
+            </div>
+            <div className="dsc-product-details-bottom">
+              <h3>R$ 5000,00</h3>
+              <h4>Computer Gamer XT</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Tenetur explicabo perferendis autem nam culpa harum iusto obcaecati eveniet
+                voluptas fugit aspernatur corporis dicta delectus voluptatum
+                soluta, dolores eaque neque? Qui!
+              </p>
+              <div className="dsc-category-container">
+                <ProductCategory />
+                <ProductCategory />
+              </div>
+            </div>
+          </div>
+    );
+}
