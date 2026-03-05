7c386fee1b0971212377079b9e3f69f59b9b1724
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-03-06 00:26:30 +0200

    Renderização da pagina de Catalogo Dinamicamente

--- frontend/src/routes/Catalog/index.tsx
+++ frontend/src/routes/Catalog/index.tsx
@@ -3,43 +3,24 @@ import './styles.css'
 import SearchBar from "../../components/SearchBar";
 import CatalogCard from "../../components/CatalogCard";
 import ButtonNextPage from "../../components/ButtonNextPage";
-import type { ProductDTO } from "../../models/products";
-
-const product: ProductDTO = {
-    id: 1,
-    name: "Smart TV",
-    price: 5000.90,
-    description: "The TV is very good, with a 4K resolution and a 60Hz refresh rate. It has a sleek design and comes with a remote control. The picture quality is excellent, with vibrant colors",
-    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources-devsuperior/master/backend/img/2-big.jpg",
-    categories: [
-        { id: 1, name: "Electrónicos" },
-        { id: 2, name: "Computadores" }
-    ]
-};
+import * as productService from "../../services/product-services";
 
 export default function Catalog() {
     return (
         <>
-            <HeaderClient/>
+            <HeaderClient />
             <main>
-                <section id="catalog-section" className="dsc-container"> 
+                <section id="catalog-section" className="dsc-container">
                     <SearchBar />
                     <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
-                        <CatalogCard product={product} />
+                        {
+                            productService.findAll().map(
+                                product => <CatalogCard key={product.id} product={product} />
+                            )
+                        }
                     </div>
 
-                    <ButtonNextPage name="Carregar mais"/>
+                    <ButtonNextPage name="Carregar mais" />
                 </section>
             </main>
         </>
